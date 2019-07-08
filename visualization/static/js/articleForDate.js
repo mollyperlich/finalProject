599eed9d9f0c43b8f22555d1fc79e8a1366b6b1b
Scott Mac McEachern <the_fixer@hotmail.com>
2019-07-08 12:00:59 -0700

    Work on setting the metadata for the selected date for calendar

--- visualization/static/js/articleForDate.js
+++ visualization/static/js/articleForDate.js
@@ -4,6 +4,7 @@ Data is stored within AWS DynamoDB and there is a AWS Lambda function that retur
 articles for a given date
 */
 
+console.log("-> articlesForData.js");
 
 //-- Const
 const ARTICLEURL = "https://1wvzwvo96a.execute-api.us-west-2.amazonaws.com/dev?searchdate=";
@@ -12,12 +13,18 @@ const ARTICLETITLESDIV = "#article-titles";
 
 const ARTICLEIMAGEDIV = "#article-image";
 
+const ARTICLE_MetadataDiv ="#article-metadatadiv";
+
+const ARTICLE_MetadataTitle = "#article-metadatatitle";
 
 
-function updateArticleDetails(searchDate){
+
+async function updateArticleDetails(searchDate, actualResult){
     /* Updates the UI based on the search date provided.
 
     Accepts : searchDate: (int) date to populate NY Time articles for; format of YYYYMMDD
+              actualResult (string) actual result for date selected, values include:
+                    "Hold", "Buy", "Sell", "Unknown"
                 
     Returns : undefined
     */
@@ -27,22 +34,74 @@ function updateArticleDetails(searchDate){
 
     //- Check Parameters
     if (Number.isInteger(searchDate) == false){
-        //- Update UI
-        let emptyArticles = [];
+        throw `Invalid search date: ${searchDate}`;
+    }
 
-        populateArticles(emptyArticles);
-
-
-        throw `Invalid search date: {searchDate}`;
+    if (actualResult == ''){
+        actualResult = "Unknown";
     }
 
 
     //- Get Articles
-    getArticlesFromEndpoint(searchDate);
+    let sourceData = await getArticlesFromEndpointAsync(searchDate);
+
+
+    //- Update Metadata
+    updateArticleMetadata(searchDate, actualResult);
+
+
+    //- Update 
 }
 
 
-function populateArticles(sourceArticles){
+
+function updateArticleMetadata(searchDate, actualResult){
+    /*
+
+    Accepts : searchDate: (int) date to populate NY Time articles for; format of YYYYMMDD
+              actualResult: (string) actual result for date selected, values include:
+                    "Hold", "Buy", "Sell", "Unknown"
+        
+    Returns : undefined
+    */
+
+    console.log("-> updateArticleMetadata");
+
+
+    //- Reference Metadata Div
+    let articleMetadataDiv = d3.select(ARTICLE_MetadataDiv);
+
+
+    //- Determine CSS Style
+    let resultStyle = "unknownresult"
+
+    if (actualResult == 'Hold'){
+        resultStyle = "holdresult";
+    }else if (actualResult == "Buy"){
+        resultStyle = "buyresult";
+    }else if (actualResult == "Sell"){
+        resultStyle = "sellresult";
+    }
+
+
+    //- Update Style
+    articleMetadataDiv.attr("class", `${resultStyle} resultcontainer`);
+
+
+    //- Reference Title Div
+    let articleTitleDiv = d3.select(ARTICLE_MetadataTitle);
+
+    //- Remove Existing
+    articleTitleDiv.selectAll("p").remove();
+
+    //- Add Title
+    let searchDateValue = moment(searchDate.toString(), "YYYYMMDD").toDate();
+
+    articleTitleDiv.append("p")
+        .text(moment(searchDateValue).format("dddd MMMM Do, YYYY"));
+}
+
+function populateArticles(sourceArticles, actualResult){
     /* Using the list of articles provided, updates the UI 
 
     Accepts : sourceArticles (array) Contains the articles for the date
@@ -51,11 +110,30 @@ function populateArticles(sourceArticles){
                 sourceurl: (string) Url of the news article on the NY Times website
                 imageurl: (string) Url to image from the news article; could be empty
                 publishdate: (int) Date when the article was published, yyyyMMdd
+              actualResult (string) actual result for date selected, values include:
+                    "Hold", "Buy", "Sell", "Unknown"
 
     Returns : undefined
     */
 
     console.log("-> populateArticles");
+
+
+
+    //- Check for Articles
+    if (sourceArticles.length == 0){
+
+        return;
+    }
+
+
+
+    //- Update Metadata
+    let articleMetadataDiv = d3.select(ARTICLE_MetadataDiv);
+
+
+
+
 
 
     //- Populate Articles List
@@ -107,31 +185,38 @@ function populateArticles(sourceArticles){
 }
 
 
-function getArticlesFromEndpoint(searchDate){
+async function getArticlesFromEndpointAsync(searchDate){
     /* Call AWS Lambda function to get data from endpoint
 
     Accepts : searchDate: (int) date to get articles for; YYYYMMDD format
 
-    Returns : Undefined
+    Returns : (array) list of the articles for the data provided
+                title: (string) title of article
+                id: (string) unique identifier of the news article; created when downloaded
+                sourceurl: (string) Url of the news article on the NY Times website
+                imageurl: (string) Url to image from the news article; could be empty
+                publishdate: (int) Date when the article was published, yyyyMMdd
     */
 
     console.log("-> getArticlesFromEndpoint");
 
-    //- Create Proxy URL
-    //  Issues setting up AWS Gateway to allow remote access use proxy
-    let sourceUrl = ARTICLEURL + searchDate;
+    try{
+
+        //- Get Data
+        let sourceUrl = ARTICLEURL + searchDate;
+
+        let sourceData = await d3.json(sourceUrl);
+
+        console.log(`Success in getting data from endpoint: ${searchDate}`);
+        console.table(sourceData)
 
 
-    d3.json(sourceUrl).then(data => {
-        console.log("Success getting articles");
-        console.table(data);
+        return sourceData;
 
-        populateArticles(data);
+    } catch(err){
+        console.log(err);
 
-    }).catch(e => {
-        console.log(`Failure getting articles: ${e}`);
-
-        let emptyArticles = [];
-        populateArticles(emptyArticles);
-    });
+        let emptyData = [];
+        return emptyData;
+    }
 }
